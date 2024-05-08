b72612f88c6c149a4f8cf1073e7da9066c0e5867
Leandro <leandro.nupsi@gmail.com>
2024-05-08 14:08:18 -0300

    Adiciona environments

--- src/app/core/interceptors/auth-github.interceptor.ts
+++ src/app/core/interceptors/auth-github.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authGithubInterceptor: HttpInterceptorFn = (req, next) => {
   const requestWithToken = req.clone({
-    headers: req.headers.set('Authorization', '')
+    headers: req.headers.set('Authorization', import.meta.env.NG_APP_GH_AUTH),
   });
 
   return next(requestWithToken);
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  GithubAuth: process.env['GH_AUTH']!,
+};
